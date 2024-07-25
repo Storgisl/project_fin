16494abd49b900f111b834fe6bcf44f4a236f5a6
Storgisl <antipov03306@list.ru>
2024-07-25 18:04:43 +0300

    test-1

---
+++ widgets/photo_button.py
@@ -0,0 +1,6 @@
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.button import Button
+
+def __init__(self, **kwargs):
+    # make sure we aren't overriding any important functionality
+    super(RootWidget, self).__init__(**kwargs)
