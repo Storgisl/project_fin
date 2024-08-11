f22286e2a7c63e100833d3e72ca69eaa74ce015b
root <root@DESKTOP-S74IRNN>
2024-08-11 16:47:48 +0300

    db_nonsence

--- widgets/layout_widget.py
+++ widgets/layout_widget.py
@@ -7,7 +7,7 @@ from kivy.uix.popup import Popup
 class RootWidget(AnchorLayout):
     def __init__(self, **kwargs):
         super(RootWidget, self).__init__(**kwargs)
-        
+
         self.add_widget(
                 Button(
                     text="Click",
@@ -18,20 +18,20 @@ class RootWidget(AnchorLayout):
     def open_filechooser(self, instance):
         # Create a FileChooserListView or FileChooserIconView
         filechooser = FileChooserListView()
-        
+
         # Create a layout to hold the filechooser and buttons
         layout = BoxLayout(orientation='vertical')
         layout.add_widget(filechooser)
-        
+
         # Create a button to close the popup
         close_button = Button(text="Close", size_hint=(1, 0.1))
         close_button.bind(on_press=self.close_popup)
         layout.add_widget(close_button)
-        
+
         # Create the popup
         self.popup = Popup(title='FileChooser', content=layout, size_hint=(0.9, 0.9))
         self.popup.open()
-    
+
     def close_popup(self, instance):
         self.popup.dismiss()
 
